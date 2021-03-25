476321a2211e9973923fa8f0b8fb696925b5e5f5
JIALIN-HUANG <78190123+JIALIN-HUANG@users.noreply.github.com>
2021-03-25 15:21:32 -0700

    update my second song

--- script.js
+++ script.js
@@ -5,10 +5,23 @@ let circle1 = document.querySelector(".circle1");
 //result of the homura's gif
 function turnhomura() {
     homura.classList.toggle("active")
-    logoD.style.display = "none";
+}
+//add an event listner
+//when someone clicks on the circle show Homura's gif
+
+circle1.addEventListener("click",turnhomura);
+
+//koi老婆！！！
+//define our second song 
+let koi = document.querySelector(".Koi");
+let circle2 = document.querySelector(".circle2");
+
+//result of the homura's gif
+function turnkoi() {
+    koi.classList.toggle("active")
 }
 
 //add an event listner
 //when someone clicks on the circle show Homura's gif
 
-circle1.addEventListener("click",turnhomura);
+circle2.addEventListener("click",turnkoi);
